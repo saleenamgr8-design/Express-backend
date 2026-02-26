285a2fbd59e0ad4bf176e63782f2143a74194738
saleena <saleenamgr8@gmail.com>
2026-02-26 09:50:38 +0545

    cors addes

--- src/app.js
+++ src/app.js
@@ -5,7 +5,12 @@ import postsRoute from "./routes/posts.route.js"
 import authRoutes from "./routes/auth.route.js"
 
 const app = express();
-
+const corsOptions = {
+    origin: process.env.CORS_ORIGIN || "*", // Allow all origins or specify your frontend URL
+    methods: "GET,POST,PUT,DELETE", // Allowed HTTP methods
+    allowedHeaders: "Content-Type,Authorization", // Allowed headers
+};
+ app.use(express.json()); 
 app.use(express.json()); // Middleware to parse JSON bodies
 
 
